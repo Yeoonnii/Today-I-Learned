65d10a8d5afa9b76116406f5e9426527ee852938
Yeoonnii <mah7128@naver.com>
2024-03-24 23:17:34 +0900

    ch07. 연산자(Operator) 1

---
+++ React/onebiteReact/section01/ch07.js
@@ -0,0 +1,7 @@
+// 비교 연산자
+// == 사용시 값만 비교함
+// === 사용시 값과 자료형 일치여부를 비교함
+console.log(1 == 1); // true
+console.log(1 == "1"); // true
+console.log(1 === "1"); // false
+console.log(1 === 1); // true

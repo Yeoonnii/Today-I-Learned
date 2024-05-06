e50c514bc3f8392ad854b770f8dbe71a552fecab
Yeoonnii <mah7128@naver.com>
2024-05-06 16:24:52 +0900

    ch04. Create - 투두 추가하기

--- React/onebiteReact/section08/src/App.jsx
+++ React/onebiteReact/section08/src/App.jsx
@@ -4,6 +4,8 @@ import List from "./component/List";
 import "./App.css";
 import { useRef, useState } from "react";
 
+// 임시데이터
+// App 컴포넌트가 리렌더링 될 때마다 다시 생성될 필요가 없고, 상수이기 때문에 값을 변경할수도 없으니 컴포넌트 외부에 선언해주어도 된다.
 const mockData = [
   { id: 0, isDone: false, content: "공부하기", date: new Date().getTime() },
   { id: 1, isDone: false, content: "빨래하기", date: new Date().getTime() },

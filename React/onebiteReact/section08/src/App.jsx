d290e2d9089fb5a1415bcc3f5cfb8ce47f7d8776
Yeoonnii <mah7128@naver.com>
2024-05-06 13:59:13 +0900

    ch03. 기능 구현 준비하기

--- React/onebiteReact/section08/src/App.jsx
+++ React/onebiteReact/section08/src/App.jsx
@@ -1,16 +1,24 @@
-import Editor from "./component/Editor"
-import Header from "./component/Header"
-import List from "./component/List"
-import "./App.css"
+import Editor from "./component/Editor";
+import Header from "./component/Header";
+import List from "./component/List";
+import "./App.css";
+import { useState } from "react";
+
+const mockData = [
+  { id: 0, isDone: false, content: "공부하기", date: new Date().getTime() },
+  { id: 1, isDone: false, content: "빨래하기", date: new Date().getTime() },
+  { id: 2, isDone: false, content: "짐정리하기", date: new Date().getTime() },
+];
 
 function App() {
+  const [todos, setTodos] = useState(mockData);
   return (
     <div className="App">
       <Header />
       <Editor />
       <List />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
